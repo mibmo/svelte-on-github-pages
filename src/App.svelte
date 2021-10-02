43d5ad45877db02896bf9d54c173cd29009b4948
mib <mib@mib.dev>
2021-10-02 21:21:10 +0200

    Add content

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,30 @@
 </script>
 
 <main>
+	<h1><a href="https://svelte.dev" alt="SvelteJS">Svelte</a> on <a href="pages.github.com" alt="Github Pages">Github Pages</a></h1>
 </main>
 
 <style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	a {
+		color: #ff3e00;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
 </style>
